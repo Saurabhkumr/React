e8d28a933ead5fe82b14ffb42d8f423826407592
Saurabh <saurabhkr0927@gmail.com>
2024-02-08 20:47:58 +0530

    basic vite and react

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>chai ban gyi</h2>
+    )
+}
+export default Chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>chai pilo</h3>
+    )
+}
+export default Chai
